9207117e8987f477baf00fd8fb103fb478c37e4b
Billiam <billiamthesecond@gmail.com>
2024-12-31 12:45:08 -0600

    Extend framecontainer instead, support padding

--- widget/shadow_container.lua
+++ widget/shadow_container.lua
@@ -1,42 +1,60 @@
 local Blitbuffer = require("ffi/blitbuffer")
-local WidgetContainer = require("ui/widget/container/widgetcontainer")
+local FrameContainer = require("ui/widget/container/framecontainer")
 
-local ShadowContainer = WidgetContainer:extend {
-  shadow_color = Blitbuffer.COLOR_GRAY,
+local Geom = require("ui/geometry")
+
+local ShadowContainer = FrameContainer:extend {
+  background = Blitbuffer.COLOR_GRAY,
   shadow_x = 5,
   shadow_y = 5,
   stripe_width = 0,
-  margin = 0,
+  padding = 0,
   radius = 0,
 }
 
 function ShadowContainer:paintTo(bb, x, y)
+  local shadow_size = self:getSize()
   local child_size = self[1]:getSize()
-  local x_pos = x + self.shadow_x + self.margin
-  local y_pos = y + self.shadow_y + self.margin
-  local w = child_size.w - self.margin * 2
-  local h = child_size.h - self.margin * 2
+
+  local shadow_x = x + self.shadow_x + self.margin + self._padding_left
+  local shadow_y = y + self.shadow_y + self.margin + self._padding_top
+
+  if not self.dimen then
+    self.dimen = Geom:new {
+      x = x,
+      y = y,
+      w = shadow_size.w,
+      h = shadow_size.h,
+    }
+  else
+    self.dimen.x = x
+    self.dimen.y = y
+  end
 
   if self.stripe_width > 0 then
     bb:hatchRect(
-      x_pos,
-      y_pos,
-      w,
-      h,
+      shadow_x,
+      shadow_y,
+      child_size.w,
+      child_size.h,
       self.stripe_width,
-      self.shadow_color
+      self.background
     )
   else
     bb:paintRoundedRect(
-      x_pos,
-      y_pos,
-      w,
-      h,
-      self.shadow_color,
+      shadow_x,
+      shadow_y,
+      child_size.w,
+      child_size.h,
+      self.background,
       self.radius
     )
   end
-  self[1]:paintTo(bb, x, y)
+  self[1]:paintTo(
+    bb,
+    x + self.margin + self._padding_left,
+    y + self.margin + self._padding_top
+  )
 end
 
 return ShadowContainer
