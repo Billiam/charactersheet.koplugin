0897b9528791d62e3f67da94837c5f2c751ff34b
Billiam <billiamthesecond@gmail.com>
2024-12-31 12:53:22 -0600

    Rename widget, reuse underline container

--- widget/input_button.lua
+++ widget/input_button.lua
@@ -1,20 +1,26 @@
 local Size = require("ui/size")
 local UnderlineContainer = require("ui/widget/container/underlinecontainer")
-local Button = require("ui/widget/button")
+local Button = require("widget/button")
 local InputDialog = require("ui/widget/inputdialog")
 local UIManager = require("ui/uimanager")
-local logger = require("logger")
 local Blitbuffer = require("ffi/blitbuffer")
+local WidgetContainer = require("ui/widget/container/widgetcontainer")
+local FrameContainer = require("ui/widget/container/framecontainer")
 
 local Device = require("device")
 local Screen = Device.screen
 
-local UnderlineInput = UnderlineContainer:extend{
+local InputButton = WidgetContainer:extend {
   value = nil,
   color = Blitbuffer.COLOR_BLACK,
+  background = Blitbuffer.COLOR_WHITE,
   bold = false,
-  linesize = Size.line.thin,
+  underline_size = Size.line.thin,
   width = Screen:scaleBySize(50),
+  bordersize = 0,
+  margin = 5,
+
+  font_size = 16,
 
   callback = nil,
   name = nil,
@@ -23,27 +29,27 @@ local UnderlineInput = UnderlineContainer:extend{
   align = "left",
 }
 
-function UnderlineInput:init()
+function InputButton:init()
   local show_hint = self.input == nil or self.input == ""
-  local text_value = show_hint and self.hint or self.input
+  local text_value = show_hint and self.hint or self.input or ""
 
-  self.button = Button:new{
-    bordersize = 0,
+  self.button = Button:new {
+    bordersize = self.bordersize,
     text = text_value,
     text_font_bold = false,
-    padding = 0,
+    padding = 3,
     align = self.align,
     width = self.width,
     margin = 0,
+    text_font_size = self.font_size,
+    background = self.background,
     callback = function()
-
       local dialog
-      dialog = InputDialog:new{
+      dialog = InputDialog:new {
         title = self.label,
         input = self.input,
         input_hint = self.hint,
         save_callback = function(value)
-
           UIManager:close(dialog)
           self:setValue(value, true)
           return false, false
@@ -55,13 +61,24 @@ function UnderlineInput:init()
   }
 
   if show_hint then
-    self.button.label_widget.fgcolor = Blitbuffer.COLOR_GRAY
+    self.button.label_widget.fgcolor = Blitbuffer.COLOR_GRAY_5
   end
 
-  self[1] = self.button
+  self[1] = FrameContainer:new {
+    margin = self.margin,
+    bordersize = 0,
+    padding = 0,
+
+    UnderlineContainer:new {
+      padding = 0,
+      linesize = self.underline_size,
+      color = self.color,
+      self.button
+    }
+  }
 end
 
-function UnderlineInput:setValue(text, refresh)
+function InputButton:setValue(text, refresh)
   self.input = text
   self.callback(self.name, text)
 
@@ -78,4 +95,4 @@ function UnderlineInput:setValue(text, refresh)
   end
 end
 
-return UnderlineInput
+return InputButton
