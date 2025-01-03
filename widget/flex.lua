5ee12dd8c456d827f7bd6e571e2c055fc2aea565
Billiam <billiamthesecond@gmail.com>
2025-01-03 13:21:39 -0600

    Allow updating span widths for space-between when dimensions change

--- widget/flex.lua
+++ widget/flex.lua
@@ -10,25 +10,40 @@ local RightContainer = require("ui/widget/container/rightcontainer")
 local TopContainer = require("ui/widget/container/topcontainer")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 
-
-local SPACE_BETWEEN = "space-between"
-local FLEX_START = "flex-start"
-local FLEX_END = "flex-end"
-local STRETCH = "stretch"
-local HORIZONTAL = "horizontal"
-local VERTICAL = "vertical"
+HorizontalGroup.id = "horizontalgroup"
+HorizontalSpan.id = "horizontalspan"
+VerticalGroup.id = "verticalgroup"
+VerticalSpan.id = "verticalspan"
+BottomContainer.id = "BottomContainer"
+CenterContainer.id = "centercontainer"
+LeftContainer.id = "leftcontainer"
+RightContainer.id = "rightcontainer"
+TopContainer.id = "topcontainer"
+BottomContainer.id = "bottomcontainer"
 
 local Flex = WidgetContainer:extend {
-  direction = HORIZONTAL,
+  direction = "horizontal",
   children = nil,
-  align_items = FLEX_START,
-  justify_content = FLEX_START,
+  align_items = "flex-start",
+  justify_content = "flex-start",
 
   width = nil,
   height = nil,
   gap = nil,
 }
 
+Flex.SPACE_BETWEEN = "space-between"
+Flex.FLEX_START = "flex-start"
+Flex.FLEX_END = "flex-end"
+Flex.STRETCH = "stretch"
+Flex.HORIZONTAL = "horizontal"
+Flex.VERTICAL = "vertical"
+Flex.CENTER = "center"
+
+local long_dimension = {
+  h = "height",
+  w = "width",
+}
 local horizontal = {
   container_class = HorizontalGroup,
   span_class = HorizontalSpan,
@@ -36,7 +51,6 @@ local horizontal = {
   flex_end_class = RightContainer,
 
   align_dimension = "h",
-  align_long = "height",
   flex_dimension = "w",
   align_start = "top",
   align_end = "bottom",
@@ -49,7 +63,6 @@ local vertical = {
   flex_end_class = BottomContainer,
 
   align_dimension = "w",
-  align_long = "width",
   flex_dimension = "h",
   align_start = "left",
   align_end = "right",
@@ -58,32 +71,32 @@ local vertical = {
 function Flex:init()
   local container_align
   local children_count = #self.children
+  self.spans = {}
 
   local size = {
     w = self.width,
     h = self.height
   }
-  local dir = self.direction == HORIZONTAL and horizontal or vertical
+  local dir = self:dir()
 
-  if self.align_items == STRETCH or self.align_items == FLEX_START then
+  if self.align_items == Flex.STRETCH or self.align_items == Flex.FLEX_START then
     container_align = dir.align_start
-  elseif self.align_items == FLEX_END then
+  elseif self.align_items == Flex.FLEX_END then
     container_align = dir.align_end
   end
 
   local max_align = 0
-  local total_flex = 0
+  self.total_flex = 0
   local gap = self.gap
-
   for _, child in ipairs(self.children) do
     local child_size = child:getSize()
-    max_align = math.max(child_size[dir.align_dimension], max_align)
-    total_flex = total_flex + child_size[dir.flex_dimension]
+    max_align = math.max(child_size[dir.align_dimension] or 0, max_align)
+    self.total_flex = self.total_flex + (child_size[dir.flex_dimension] or 0)
   end
 
   if not gap then
-    if children_count > 1 and self.justify_content == SPACE_BETWEEN then
-      gap = (size[dir.flex_dimension] - total_flex) / (children_count - 1)
+    if children_count > 1 and self.justify_content == Flex.SPACE_BETWEEN and size[dir.flex_dimension] then
+      gap = (size[dir.flex_dimension] - self.total_flex) / (children_count - 1)
     else
       gap = 0
     end
@@ -93,40 +106,69 @@ function Flex:init()
   if self.children then
     for i, child in ipairs(self.children) do
       table.insert(elements, child)
-      if self.align_items == STRETCH then
-        child[dir.align_long] = max_align
+
+      if self.align_items == Flex.STRETCH then
+        local long_flex_key = long_dimension[dir.align_dimension]
+        local original_value = child[long_flex_key]
+        child[long_flex_key] = max_align
+
+        if original_value ~= max_align and child.updateFlex then
+          child:updateFlex(long_flex_key)
+        end
       end
+
+
       if i ~= children_count then
-        table.insert(elements, dir.span_class:new {
+        local span = dir.span_class:new {
           width = gap
-        })
+        }
+        table.insert(elements, span)
+        table.insert(self.spans, span)
       end
     end
   end
 
   local wrapper_class
-  if self.justify_content == FLEX_START or self.justify_content == SPACE_BETWEEN then
+  if self.justify_content == Flex.FLEX_START or self.justify_content == Flex.SPACE_BETWEEN then
     wrapper_class = dir.flex_start_class
-  elseif self.justify_content == FLEX_END then
+  elseif self.justify_content == Flex.FLEX_END then
     wrapper_class = dir.flex_end_class
   else
     wrapper_class = CenterContainer
   end
 
-  local container = dir.container_class:new {
+  self.container = dir.container_class:new {
     align = container_align,
     table.unpack(elements)
   }
-  self.children = nil
-  local container_size = container:getSize()
+  local container_size = self.container:getSize()
 
   self[1] = wrapper_class:new {
     dimen = {
       [dir.align_dimension] = container_size[dir.align_dimension],
-      [dir.flex_dimension] = size[dir.flex_dimension]
+      [dir.flex_dimension] = size[dir.flex_dimension] or container_size[dir.flex_dimension]
     },
-    container
+    self.container
   }
+end
+
+function Flex:updateFlex(changed_dimension)
+  local dir = self:dir()
+  self.container:resetLayout()
+
+  if self.justify_content == Flex.SPACE_BETWEEN and long_dimension[dir.flex_dimension] == changed_dimension then
+    if self[changed_dimension] then
+      local new_gap = (self[changed_dimension] - self.total_flex) / (#self.children - 1)
+
+      for _, span in ipairs(self.spans) do
+        span.width = new_gap
+      end
+    end
+  end
+end
+
+function Flex:dir()
+  return self.direction == Flex.HORIZONTAL and horizontal or vertical
 end
 
 return Flex
